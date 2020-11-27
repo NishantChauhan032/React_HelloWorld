06ef94164f69870eb3f09a782ddbc0824678a12b
NishantChauhan <nishantkr0210@gmail.com>
2020-11-27 22:48:06 +0530

    [NISHANT] add: ability to use one way data binding to display welcome message

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import React from 'react';
 import logo from './assets/fblogo.png'
 
 class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      title: 'Hello From Capgemini Team!!'
+    }
+  }
   render(){
     return(
       <div>
-        <h1>Hello From Facebook!!</h1>
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
