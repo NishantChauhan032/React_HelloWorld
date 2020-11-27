9fb82bb280001debfc09fba3e57d19e3f0c25b5c
NishantChauhan <nishantkr0210@gmail.com>
2020-11-27 23:26:25 +0530

    [NISHANT] add: Event binding technique used

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import logo from './assets/capg.png'
 
 class App extends React.Component{
 
+  url = 'http://capgemini.com'
   constructor(){
     super()
     this.state = {
       title: 'Hello From Capgemini Team!!'
     }
   }
+
+  onClick = ($event) =>{
+    console.log("Enter button is clicked!" , $event);
+    window.open(this.url , "_blank");
+  }
+
   render(){
     return(
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="Capgemini Logo!" />
+        <img src={logo} onClick={this.onClick} alt="Capgemini Logo!" />
       </div>
     );
   }
